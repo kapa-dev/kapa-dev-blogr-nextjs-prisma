cea90586251fc4bbaf6520219cf2a5057b4e0892
Kenneth Alvin Abad <kpabad.halcyondigital@gmail.com>
2023-10-17 15:43:04 +0800

    fix: github oauth secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -29,7 +29,7 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
   adapter: PrismaAdapter(prisma),
-  secret: process.env.SECRET,
+  secret: process.env.NEXTAUTH_SECRET || process.env.SECRET,
 };
 
 export default NextAuth(authOptions);
